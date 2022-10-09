041b6770d648bc825570dbb8114dc69cb41b4619
kalkidanh <kalkidanh89@gmail.com>
2022-10-09 09:07:56 -0700

    monty

---
+++ main.c
@@ -0,0 +1,87 @@
+#include "monty.h"
+
+int stack_queue = 1;
+/**
+ * main - main loop of the monty bytecode interpreter
+ * @argc: number of command line args
+ * @argv: command line args
+ * Return: 0 on success, otherwise exits with failure code
+ */
+int main(int argc, char *argv[])
+{
+	size_t nb;
+	unsigned int len;
+	ssize_t read;
+	char *buf = NULL, *opcode = NULL;
+	stack_t *stack = NULL;
+	FILE *file;
+
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	for (len = 1; (read = getline(&buf, &nb, file)) != -1; len++)
+	{
+		if (buf[read - 1] == '\n')
+			buf[read - 1] = '\0';
+		opcode = strtok(buf, " ");
+		if (opcode && *opcode != '#')
+			call_opcode(opcode, &stack, len);
+	}
+	free(buf);
+	free_stack(&stack);
+	fclose(file);
+	return (0);
+}
+
+/**
+ * call_opcode - gets the opcode and makes the appropriate op_function call
+ * @opcode: opcode string
+ * @stack: the pointer to the pointer to the stack for operations
+ * @line: line number for the instruction read
+ * Return: void (no return)
+ */
+void call_opcode(char *opcode, stack_t **stack, unsigned int line)
+{
+	instruction_t instructions[] = {
+		{"push", push_op},
+		{"pall", pall_op},
+		{"pint", pint_op},
+		{"pop", pop_op},
+		{"swap", swap_op},
+		{"add", add_op},
+		{"nop", nop_op},
+		{"sub", sub_op},
+		{"div", div_op},
+		{"mul", mul_op},
+		{"mod", mod_op},
+		{"pchar", pchar_op},
+		{"pstr", pstr_op},
+		{"rotl", rotl_op},
+		{"rotr", rotr_op},
+		{"queue", queue_op},
+		{"stack", stack_op}
+	};
+	unsigned int i;
+
+	for (i = 0; i < 17; i++)
+	{
+		if (strcmp(instructions[i].opcode, opcode) == 0)
+			break;
+	}
+	if (i < 17)
+		(*instructions[i].f)(stack, line);
+	else
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line, opcode);
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ monty.h
@@ -0,0 +1,60 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void call_opcode(char *opcode, stack_t **stack, unsigned int line);
+void push_op(stack_t **stack, unsigned int line_number);
+void pall_op(stack_t **stack, unsigned int line_number);
+void pint_op(stack_t **stack, unsigned int line_number);void pop_op(stack_t **stack, unsigned int line_number);
+void swap_op(stack_t **stack, unsigned int line_number);
+void add_op(stack_t **stack, unsigned int line_number);
+void nop_op(stack_t **stack, unsigned int line_number);
+void sub_op(stack_t **stack, unsigned int line_number);
+void div_op(stack_t **stack, unsigned int line_number);
+void mul_op(stack_t **stack, unsigned int line_number);
+void mod_op(stack_t **stack, unsigned int line_number);
+void pchar_op(stack_t **stack, unsigned int line_number);
+void pstr_op(stack_t **stack, unsigned int line_number);
+void rotl_op(stack_t **stack, unsigned int line_number);
+void rotr_op(stack_t **stack, unsigned int line_number);
+void queue_op(stack_t **stack, unsigned int line_number);
+void stack_op(stack_t **stack, unsigned int line_number);
+stack_t *pop(stack_t **stack);
+stack_t *pop_end(stack_t **stack);
+stack_t *push(stack_t **stack, int val);
+stack_t *enqueue(stack_t **stack, int val);
+void free_stack(stack_t **stack);
+extern int stack_queue;
+#endif
